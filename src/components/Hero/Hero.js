d730bbd31cba10d0fe5a42410f5475b27eb456c8
biraSRT <95512405+biraSRT@users.noreply.github.com>
2022-03-06 23:40:27 -0500

    second commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section>
+    <LeftSection>
+      <SectionTitle>
+        Welcome To My Portfolio
+      </SectionTitle>
+      <SectionText>
+        put text here.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend, DiNpm} from 'react-icons/di';
+import { AiFillHtml5 } from "react-icons/ai";
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider/>
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      Here is a list of Technologies I have worked with so far.<br/>
+      My knowledge is of course not limited to any of these technologies.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <AiFillHtml5 size="3rem"/>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+);
+
+export default Technologies;
